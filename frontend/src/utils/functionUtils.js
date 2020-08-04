6fcdfb6c29b4880dd9733c9b89d9260b3fade10c
javadev-jef <jefersom.andradequeiroz@gmail.com>
2020-08-04 08:13:42 -0300

    FunctionUtils: added 2 functions date of month

--- frontend/src/utils/functionUtils.js
+++ frontend/src/utils/functionUtils.js
@@ -1,4 +1,16 @@
+import moment from "moment";
+
 export const formatCurrencyValue = (value) =>
 {
     return Intl.NumberFormat("pt-BR", {style: "currency", currency: "BRL"}).format(value);
+}
+
+export const getInitialDateOfMonth = () =>
+{
+    return moment().startOf('month');
+}
+
+export const getFinalDateOfMonth = () =>
+{
+    return moment().endOf('month');
 }
