bd9aba471a6697146e46da147c084270a239ea41
javadev-jef <jefersom.andradequeiroz@gmail.com>
2020-08-04 07:53:19 -0300

    StatementListTable all refactored with Material UI

--- frontend/src/components/StatementListTable/index.js
+++ frontend/src/components/StatementListTable/index.js
@@ -1,62 +1,71 @@
 import React from "react";
-import  {Table}  from 'rsuite';
-import {LOCALE_RSTABLE} from "../../constants";
+import MUIDataTable from "mui-datatables";
+import {createMuiTheme, MuiThemeProvider} from "@material-ui/core/styles"
+import { formatCurrencyValue } from "../../utils/functionUtils";
+import "./style.scss";
+import { CircularProgress, Backdrop } from "@material-ui/core";
+import { TABLE_THEME, TABLE_TEXT_LABEL, DOWNLOAD_TABLE_CONFIG, DOWNLOAD_TABLE_OPTIONS } from "../../constants/constants";
 
 const StatementListTable = ({statements}) =>
 {
-    const isDebit = (rawData) =>
+    const theme = createMuiTheme(TABLE_THEME);
+
+    const valueColumns = {
+        customBodyRender: value => formatCurrencyValue(value),
+    };
+
+    const headers = [
+        {name: "description", label: "Descrição"},
+        {name: "date", label: "Data"},
+        {name: "type", label: "Tipo"},
+        {name: "value", label: "Valor", options:{...valueColumns}},
+        {name: "value", label: "Saldo", options:{...valueColumns}}
+    ];
+
+    const options = 
     {
-        return rawData["type"] === "Débito";
-    } 
+        sort: false,
+        filter: false,
+        search: false,
+        rowHover: false,
+        fixedHeader: true,
+        selectableRows: "none",
+        responsive: "standard",
+        tableBodyHeight: "460px",
+        downloadOptions: DOWNLOAD_TABLE_OPTIONS("Extrato - BlueBank.csv"),
+        onDownload: DOWNLOAD_TABLE_CONFIG,
+        textLabels: TABLE_TEXT_LABEL,
+        setRowProps: (row, dataIndex, rowIndex) => 
+        { 
+            return row[2] === "Débito" && {style: {backgroundColor: "#ffe6e6", color: "#FF0000"}} 
+        },
+    };
 
-    const CustomCell = ({rowData, rowIndex, dataKey, ...props}) => 
-    (
-        <Table.Cell {...props} style={isDebit(rowData) ? {color: "red"} : {}}>
-            {dataKey ? rowData[dataKey] : rowIndex + 1}
-        </Table.Cell>
-    );
 
-    const NumberFormatCustomCell = ({rowData, rowIndex, dataKey, ...props}) =>
-    (
-        <Table.Cell {...props} style={isDebit(rowData) ? {color: "red"} : {}}>
-            {dataKey && Intl.NumberFormat("pt-BR", {style: "currency", currency: "BRL"}).format(rowData[dataKey])}
-        </Table.Cell>
-    );
+    const backdropStyle = {
+        zIndex: 101, 
+        position: "absolute", 
+        flexDirection: "column",
+        background: "rgba(255, 255, 255, 0.5)", 
+        borderRadius: "8px 8px 0 0",
+    }
 
     return(
-        <>
-        <Table height={400} data={statements.data} loading={statements.loading} headerHeight={48} locale={LOCALE_RSTABLE}>
-            <Table.Column width={60} align="center">
-                <Table.HeaderCell>#</Table.HeaderCell>
-                <CustomCell />
-            </Table.Column>
-
-            <Table.Column width={400} fixed>
-                <Table.HeaderCell>Descrição</Table.HeaderCell>
-                <CustomCell dataKey="description"/>
-            </Table.Column>
-
-            <Table.Column width={160}>
-                <Table.HeaderCell align="left">Data</Table.HeaderCell>
-                <CustomCell dataKey="date"/>
-            </Table.Column>
-
-            <Table.Column width={100}>
-                <Table.HeaderCell>Tipo</Table.HeaderCell>
-                <CustomCell dataKey="type"/>
-            </Table.Column>
-
-            <Table.Column width={150}>
-                <Table.HeaderCell>Valor</Table.HeaderCell>
-                <NumberFormatCustomCell dataKey="value"/>
-            </Table.Column>
-
-            <Table.Column width={150}>
-                <Table.HeaderCell>Saldo</Table.HeaderCell>
-                <NumberFormatCustomCell dataKey="value"/>
-            </Table.Column>
-        </Table>
-        </>
+        <div style={{position: "relative"}}>
+            <Backdrop open={statements.loading} style={backdropStyle}>
+                <CircularProgress style={{color: "#0091EA", marginTop: "64px", marginBottom: "12px"}}/>
+                <span>Carregando...</span>
+            </Backdrop>
+            <MuiThemeProvider theme={theme}>
+                <MUIDataTable
+                    title={"Extrato"}
+                    data={statements.data}
+                    columns={headers}
+                    options={statements.loading ? {...options, customRowRender: () => null} : {...options, customRowRender: null}}
+                    className="mui-datatable-custom"
+                />
+            </MuiThemeProvider>
+        </div>
     );
 }
 
