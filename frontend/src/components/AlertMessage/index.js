30f3158e493179eed0ac0b35572058c84b2da776
javadev-jef <jefersom.andradequeiroz@gmail.com>
2021-01-23 19:03:54 -0300

    Fix: AlertMessage hiding on any click on the page.

--- frontend/src/components/AlertMessage/index.js
+++ frontend/src/components/AlertMessage/index.js
@@ -4,13 +4,23 @@ import {Alert} from '@material-ui/lab';
 
 const AlertMessage = ({maxWidth, anchorOrigin, autoHideDuration, open, onClose, severity, message}) =>
 {
+    const handleClose = (event, reason) => 
+    {
+        if (reason === 'clickaway') 
+        {
+          return;
+        }
+
+        onClose();
+    };
+    
     return(
         <Snackbar
             style={{maxWidth: maxWidth}}
             anchorOrigin={anchorOrigin}
             autoHideDuration={autoHideDuration}
             open={open}
-            onClose={onClose}
+            onClose={handleClose}
         >
                 <Alert elevation={6} variant="filled" severity={severity}>
                     {message}
